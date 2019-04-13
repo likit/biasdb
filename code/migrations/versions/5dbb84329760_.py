ad084033e064d7c3979cae6dd1a547b5d5b408dc
Likit Preeyanon <likit.pre@mahidol.edu>
2019-04-13 10:26:56 +0700

    added pyc to the gitignore added some revisions

---
+++ code/migrations/versions/5dbb84329760_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 5dbb84329760
+Revises: d3c42c6d13d0
+Create Date: 2019-04-13 10:16:18.295599
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5dbb84329760'
+down_revision = 'd3c42c6d13d0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ code/migrations/versions/d3c42c6d13d0_initial_revision.py
@@ -0,0 +1,24 @@
+"""initial revision
+
+Revision ID: d3c42c6d13d0
+Revises: 
+Create Date: 2019-04-13 09:57:47.914154
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd3c42c6d13d0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

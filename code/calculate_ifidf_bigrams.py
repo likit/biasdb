10b53f982e3ed14297bae686085f7b6fdadd6f4e
Likit Preeyanon <likit.pre@mahidol.edu>
2019-04-17 15:00:18 +0700

    added code for creating bigrams from abstracts and calculate Tfidf

---
+++ code/calculate_ifidf_bigrams.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.sql import select, update
+from textblob import TextBlob
+from pandas import DataFrame
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
+
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+
+engine = create_engine('postgres+psycopg2://postgres:{}@pg/bactwatch'.format(POSTGRES_PASSWORD))
+connection = engine.connect()
+
+metadata = MetaData()
+
+article_tbl = Table('articles', metadata, autoload=True, autoload_with=engine)
+bigram_tbl = Table('bigrams', metadata, autoload=True, autoload_with=engine)
+
+en_stopwords = stopwords.words('english')
+
+def drop_stopwords(text):
+    if text:
+        return ' '.join([t.lower() for t in text.split() if t not in en_stopwords])
+    else:
+        return ''
+
+
+def calculate():
+    result_proxy = connection.execute(select([article_tbl.c.id, article_tbl.c.data['AB']])).fetchall()
+    all_articles = DataFrame(result_proxy, columns=['pid', 'abstract'])
+    all_articles['cleaned_abstract'] = all_articles['abstract'].apply(drop_stopwords)
+
+    vectorizer = TfidfVectorizer(stop_words='english', ngram_range=(2,2), analyzer='word', min_df=1, max_df=0.8)
+    vectorizer.fit(all_articles['cleaned_abstract'])
+    for k, v in vectorizer.vocabulary_.items():
+        bigram = connection.execute(select([bigram_tbl]).where(bigram_tbl.c.text==k)).first()
+        if bigram:
+            u = update(bigram_tbl).values(
+                tfidf=vectorizer.idf_[v]
+            ).where(bigram_tbl.c.id==bigram.id)
+            rp = connection.execute(u)
+
+if __name__ == '__main__':
+    calculate()
+
---
+++ code/create_bigrams.py
@@ -0,0 +1,66 @@
+import os
+import sys
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.sql import select, insert
+from textblob import TextBlob
+from pandas import DataFrame
+from collections import defaultdict
+from nltk.corpus import stopwords
+
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+
+engine = create_engine('postgres+psycopg2://postgres:{}@pg/bactwatch'.format(POSTGRES_PASSWORD))
+connection = engine.connect()
+
+metadata = MetaData()
+
+article_tbl = Table('articles', metadata, autoload=True, autoload_with=engine)
+bigram_tbl = Table('bigrams', metadata, autoload=True, autoload_with=engine)
+organism_tbl = Table('organisms', metadata, autoload=True, autoload_with=engine)
+
+en_stopwords = stopwords.words('english')
+
+def drop_stopwords(text):
+    if text:
+        return ' '.join([t.lower() for t in text.split() if t not in en_stopwords])
+    else:
+        return ''
+
+
+def create_bigrams(ngram=2):
+    organisms = connection.execute(select([organism_tbl]))
+    spnames = set([org.species for org in organisms])
+    bigrams = defaultdict(list)
+    print('Loading articles and generating bigrams..')
+    result_proxy = connection.execute(select([article_tbl.c.id, article_tbl.c.data['AB']])).fetchall()
+    all_articles = DataFrame(result_proxy, columns=['pid', 'abstract'])
+    all_articles['cleaned_abstract'] = all_articles['abstract'].apply(drop_stopwords)
+
+    for i in range(len(all_articles)):
+        row = all_articles.loc[i]
+        for np in TextBlob(row.cleaned_abstract).noun_phrases:
+            for bg in TextBlob(np).ngrams(ngram):
+                bigram = ' '.join(bg).lower()
+                if bigram not in spnames:
+                    bigrams[bigram].append(row.pid)
+        if i % 100 == 0:
+            print('...{}'.format(i))
+
+    print('Saving bigrams..')
+    for n, bg in enumerate(bigrams, start=1):
+        for pid in bigrams[bg]:
+            ins = insert(bigram_tbl).values(
+                article_id=pid.item(),
+                text=bg,
+            )
+            rp = connection.execute(ins)
+        if n % 1000 == 0:
+            print('...{}'.format(n))
+
+
+if __name__ == '__main__':
+    try:
+        ngram = int(sys.argv[1])
+    except IndexError:
+        ngram = 2
+    create_bigrams(ngram)
